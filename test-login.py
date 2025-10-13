2f7a80bdc23db0eb03f0022cf87c16bd76dd8dcf
Rodriguez Jorge <rodriguezja10@gmail.com>
2025-10-13 20:39:26 -0300

    Aumentamos tiempo en el codigo

--- test-login.py
+++ test-login.py
@@ -1,5 +1,6 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+import time
 
 
 def test_login():
@@ -7,12 +8,17 @@ def test_login():
 
     try:
         driver.get("https://www.saucedemo.com/")
+
+        time.sleep(5)
+        
         # indicamos el usuario para login
         driver.find_element(By.ID,"user-name").send_keys("standard_user")
         # indicamos el pass para el login
         driver.find_element(By.ID,"password").send_keys("secret_sauce")
         # hacemos click en el boton
         driver.find_element(By.ID,"login-button").click()
+
+        time.sleep(5)
 
 
     except Exception as e:
