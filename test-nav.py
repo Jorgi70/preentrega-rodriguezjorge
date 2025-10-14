3f66771938d3309583e76cc8deb75fa0da4d13a6
Rodriguez Jorge <rodriguezja10@gmail.com>
2025-10-14 18:50:46 -0300

    Se agrego prueba de titulo y se probo el mismo con EXITO

--- test-nav.py
+++ test-nav.py
@@ -2,24 +2,25 @@ from selenium import webdriver
 import time
 # importamos modulo de selenium para los selectores
 from selenium.webdriver.common.by import By
+def test_nav():
+    #inicializamos el navegador Chrome
+    driver = webdriver.Chrome()
+    # analiza o valida el selenium implicitly_wait(espera elemento de la web)
+    driver.implicitly_wait(7)
 
-#inicializamos el navegador Chrome
-driver = webdriver.Chrome()
-# analiza o valida el selenium implicitly_wait(espera elemento de la web)
-driver.implicitly_wait(7)
+    try:
+        # Ingresamos a la Pagina
+        driver.get("https://www.saucedemo.com/")
+        #  Validamos el Titulo de la misma
+        titulo = driver.find_element(By.CLASS_NAME,"login_logo")
+        # Aca validamos el titulo
+        assert titulo.text == "Swag Labs", f"Texto Esperado: 'Swag Labs', pero se encontro {titulo.text}"
 
-try:
-    # Ingresamos a la Pagina
-    driver.get("https://www.saucedemo.com/")
-    #  Validamos el Titulo de la misma
-    titulo = driver.find_element(By.CLASS_NAME,"login_logo")
-    assert titulo == "Swag Labs"
+        print("Titulo Validado con EXITO")
 
-    print("Titulo Validado con EXITO")
+    except Exception as e:
+        print(f"El error fue: {e}")
+        raise
 
-except Exception as e:
-    print(f"El error fue: {e}")
-    raise
-
-finally:
-    driver.quit()
+    finally:
+        driver.quit()
